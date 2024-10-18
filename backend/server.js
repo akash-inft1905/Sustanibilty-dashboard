afc86c2b4c720b1cdeee38da8a5239d6710f27f0
Akash <akashmaurya000001@gmail.com>
2024-10-19 00:30:44 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://sustanibilty-dashboard-oren.onrender.com/",
+  ],
 };
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
